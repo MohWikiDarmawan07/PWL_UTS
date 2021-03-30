857852be22ef81b6dcac07b4af40977235d61244
MohWikiDarmawan07 <wiki.error07@gmail.com>
2021-03-30 10:01:42 +0700

    menyeting nilai $fillable

--- app/Models/Barang.php
+++ app/Models/Barang.php
@@ -2,10 +2,24 @@
 
 namespace App\Models;
 
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Model;
 
 class Barang extends Model
 {
-    use HasFactory;
-}
+    protected $table = "barangs";
+    public $timestamp = false;
+    protected $primarykey = 'id_barang';
+    protected $fillable = [
+        'id_barang',
+        'kode_barang',
+        'nama_barang',
+        'kategori_barang',
+        'harga',
+        'qty'
+    ];
+    
+};
